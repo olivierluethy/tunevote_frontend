42bcf8e309bd246e91ceb8085ea518b262aeff81
BaskLash <olivier.luethy@gmx.net>
2025-10-20 23:06:41 +0200

    Create page for creating sessions and queue, overall dashboard user experience

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Scans all JS, JSX, TS, and TSX files in the src directory
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
