880cfafaa2934970ea1768a4e01bf60e12a6299d
BaskLash <olivier.luethy@gmx.net>
2025-10-29 22:24:48 +0100

    Remove double slash in API requests

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,7 +19,7 @@ export default function Register() {
     setSuccess(false);
 
     try {
-      const res = await axios.post("https://tunevote.com//register", {
+      const res = await axios.post("https://tunevote.com/register", {
         username,
         email,
         password,
