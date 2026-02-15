22b93aa8eea00746e23bcd6c9abb8c2d60f465f9
BaskLash <olivier.luethy@gmx.net>
2026-02-15 15:47:26 +0100

    Can't find the song button

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -22,7 +22,7 @@ export default function Register() {
     setSuccess(false);
 
     try {
-      const res = await axios.post("https://api.tunevote.com/register", {
+      const res = await axios.post("http://localhost:4000/register", {
         email,
         password,
       });
@@ -44,11 +44,11 @@ export default function Register() {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = "https://api.tunevote.com/auth/google";
+    window.location.href = "http://localhost:4000/auth/google";
   };
 
   const handleFacebookLogin = () => {
-    window.location.href = "https://api.tunevote.com/auth/facebook";
+    window.location.href = "http://localhost:4000/auth/facebook";
   };
 
   return (
